3841e257da4a9ecb9cac608667009b0ef2ac487d
JamesLeach1999 <55150819+JamesLeach1999@users.noreply.github.com>
2020-05-31 14:14:03 +0100

    Final product without CSS
    Sanitises data, adds markers to map with heatmap representing IMD values and processes txt, csv and xlsx files

--- php/index.php
+++ php/index.php
@@ -1,20 +1,9 @@
-<?php
-//  require_once("../private/initialize.php");
-// include_once("../php/private/models/post.php"); ?>
 
-
-<?php
-$test = "numberwang";
-echo file_exists("/private/api/post/post.php");
-// $query = $_POST['postcode'];
-
-
-?>
 
 <div id="content">
 
-<form action="/Trust impact/php/private/api/post/read.php" method="post">
-    <input type="text" name="postcode" multiple>
+<form action="/Trust impact/php/private/api/post/read.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="postcode" id="postcode">
     <input type="submit" value="submit" name="submit">
 </form>
 
--- php/private/api/post/read.php
+++ php/private/api/post/read.php
@@ -3,13 +3,77 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json");
 
+include_once "../../../SimpleXLSX.php";
 include_once "../../config/Database.php";
 include_once "../../models/post.php";
-// include_once "../../../index.php";
-// only process if submit is set
-if(isset($_POST['postcode'])){
-    $postie = $_POST['postcode'];
-};
+
+$results = [];
+$file = $_FILES['postcode']['tmp_name'];
+$filename = $_FILES['postcode']['name'];
+
+$row = 1;
+
+$fileinfo = pathinfo($filename, PATHINFO_EXTENSION);
+var_dump($_FILES);
+
+if($fileinfo == "xlsx"){
+$allowed = array("Post code", "postcode", "POSTCODE", "Post Code", "PostCode", "Postcode");
+
+if ( $xlsx = SimpleXLSX::parse($file) ) {
+    // Produce array keys from the array values of 1st array element
+	$header_values = $rows = [];
+	foreach ( $xlsx->rows() as $k => $r ) {
+        if ( $k === 0 ) {
+            $header_values = $r;
+			continue;
+        }
+        
+        // get the individual header titles and compare to array of postcodes to see if theres a match
+        for($i = 0; $i < count($header_values); $i++){
+            if(in_array($header_values[$i], $allowed)){
+                
+                array_push($results, $r[$i]);
+            }
+            
+        }
+        // var_dump($header_values[$k]);
+	}
+}
+} else if ($fileinfo == "csv"){
+    if (($handle = fopen($file, "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $num = count($data);
+            $row++;
+            for ($c=0; $c < $num; $c++) {
+                echo $data[$c] . "<br />\n";
+                $results[] = $data[$c];
+                $keys = array_keys($data);
+        }
+    }
+    fclose($handle);
+}
+} else if ($fileinfo == "txt"){
+    $lines = file($file, FILE_IGNORE_NEW_LINES);
+
+    // var_dump($lines);
+    foreach($lines as $l){
+        array_push($results, $l);
+    }
+
+} else {
+    echo "Please upload files in csv, txt or xlsx format";
+    exit();
+}
+
+// var_dump($results);
+// seperating each value to a string to be read by sql
+$array = array();
+
+foreach($results as $p){
+    $p = "'" . $p . "'";
+    array_push($array, $p);
+}
+$com_seperated = implode(",", $array);
 
 // instantiate db and connect
 
@@ -22,10 +86,16 @@ $db = $database->connect();
 $post = new Post($db);
 
 // query
-$result = $post->read($postie);
+$result = $post->read($com_seperated);
 
 $num = $result->rowCount();
+
+foreach($results as $res){
+    $q[] = $res;
+}
+// var_dump($q);
 if($num > 0){
+    print_r($res);
     // post array
     $posts_arr = array();
     $posts_arr['data'] = array();
@@ -42,9 +112,7 @@ if($num > 0){
     
         // push to "data"
         array_push($posts_arr['data'], $post_item);
-    }
-    // header("Location: ../../../maps.html");
-    
+    }    
     
     // turn to json
     echo json_encode($posts_arr);
@@ -58,7 +126,5 @@ fwrite($fp, json_encode($posts_arr));
 fclose($fp);
 // redirect to maps
 header("Location: ../../../maps.html");
-// str.replace("/<\/?[^>]+>/gi", '');
-
 
 // check if any post
