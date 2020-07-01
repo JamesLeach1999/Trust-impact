ce6e6ec8c841dc2e0d77f16098f9ffe3017b9dfa
JamesLeach1999 <55150819+JamesLeach1999@users.noreply.github.com>
2020-07-01 17:09:46 +0100

    Final changes to the display page

--- resources/views/pages/store.blade.php
+++ resources/views/pages/store.blade.php
@@ -4,13 +4,13 @@
   <head>
     <title>Simple Map</title>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <script
-      src="https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap"
+    {{-- <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC6xrYHhT-_CeoktqgAwGjbOCNrmVUkXno&callback=initMap"
       defer
-    ></script>
+    ></script> --}}
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 
-
+{{-- <h1>{{$posts}}</h1> --}}
     <style type="text/css">
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
@@ -31,7 +31,7 @@
       (function(exports) {
 
         "use strict";
-        // initialising map
+
         function initMap() {
           exports.map = new google.maps.Map(document.getElementById("map"), {
             center: {
@@ -41,18 +41,17 @@
             },
             zoom: 12
           });
-         
+          
+
+          
         }
-        // exports map to the current window
+
         exports.initMap = initMap;
       })((this.window = this.window || {}));
     </script>
-    // only accesses the values needed for the circle and geo location
 @foreach($res as $posts => $t)
     
-
     <script>
-    // accessing the values sent by parseFile through arrow notation
     var a = {!!json_encode($t->postcodes)!!};
     
   
@@ -63,14 +62,17 @@
 
                 null,
                 function (data) {
-                  // have to access this twice, once for the api call and once for the results
+                  console.log(data);
                   var j = {!!json_encode($t->postcodes)!!};
 
                   var p = data.results[0].geometry.location;
                   var cityCircle;
-                  
+                  var q = p;
 
                   var b = {!!json_encode($t->imd)!!};
+                  
+                  // still in the loop, sets heat map array and lat long for each result
+                  
 
                   var imd = b;
 
@@ -81,22 +83,18 @@
                   if (imd > 30000) {
                     decile = 10;
                     color = "#17FF17";
-
                   } else if (imd > 27000) {
                     color = "#40FF14";
                     decile = 9;
                   } else if (imd > 24000) {
                     color = "#70FF14";
                     decile = 8;
-
                   } else if (imd > 21000) {
                     color = "#A0FF14";
                     decile = 7;
-
                   } else if (imd > 18000) {
                     color = "#D0FF14";
                     decile = 6;
-
                   } else if (imd > 15000) {
                     decile = 5;
                     var color = "#FFFD14";
@@ -114,17 +112,17 @@
                     var color = "#FF6D14";
                   } else {
                     decile = "";
-                    var color = "#FF141A";
+                    var color = "rgba(255, 0, 0, 0.05)";
                   }
 
                   console.log(color);
-                  // initialising and filling out details of the circle
+
                   var cityCircle = new google.maps.Circle({
                     strokeColor: "#FF0000",
-                    strokeOpacity: 2,
-                    strokeWeight: 0.3,
+                    strokeOpacity: 0.5,
+                    strokeWeight: 0.1,
                     fillColor: color,
-                    fillOpacity: 0.5,
+                    fillOpacity: 0.4,
                     map: map,
                     center: p,
                     radius: 200,
@@ -134,7 +132,7 @@
                       "<br><strong>Decile: </strong>" +
                       decile,
                   });
-                  // setting a small info window when you hover over the circle
+
                   var infoWindow = new google.maps.InfoWindow();
 
                   google.maps.event.addListener(
@@ -159,8 +157,6 @@
                   );
                 
           });
-
-
     
     </script>
     @endforeach
@@ -170,6 +166,6 @@
     <div id="map"></div>
   </body>
   <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC6xrYHhT-_CeoktqgAwGjbOCNrmVUkXno&callback=initMap">
     </script>
 </html>
